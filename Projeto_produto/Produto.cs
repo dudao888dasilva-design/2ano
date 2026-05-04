144a814f8fa0008a41a56860d3581d0d0e2d1fbb
Maria Silva <dudao.888.dasilva@gmail.com>
2026-05-04 14:44:30 -0300

    Create Produto.cs

---
+++ Projeto_produto/Produto.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SENAI01
+{
+    internal class Produto
+    {
+        public string nome;
+        public double preço;
+        public int quantidade;
+
+
+        public double ValorTotalemEstoque()
+        {
+            return preço * quantidade;
+        }
+        public void AdicionarProduto(int quantidade)
+        {
+            quantidade += quantidade;
+        }
+        public void RemoverProduto(int quantidade)
+        {
+            quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return nome
+            + ", $ "
+            + preço.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + quantidade
+            + "Unidade, Total: $ "
+            + ValorTotalemEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
